c8c3ab25e223a872a188a11ded66ae70d007b961
dekateayush2-crypto <dekateayush2@gmail.com>
2025-09-13 21:21:02 +0530

    Add files via upload

---
+++ taask1.py
@@ -0,0 +1,8 @@
+Enter the first number: 8
+Enter the first number: 2
+
+Expected Output:
+The adittion of 8 and 2 is: 10
+the multiplication of 8 and 2 is: 6
+the substration of 8 and 2 is: 6
+the division of 8 and 2 is: 4
